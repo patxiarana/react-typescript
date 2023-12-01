7a6e8bb4601da3ce22a4fd511b374f2f40f8e184
unknown <patxi05@gmal.com>
2023-12-01 15:17:46 -0300

    modify types

--- react-typescript/src/components/Form.tsx
+++ react-typescript/src/components/Form.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 const Form = () => {
+
+
+    const [inputValues, setInputValues] = useState({
+        nick: '',
+        subMonths: 0,
+        avatar: '',
+        description: '',
+    })
     const handlesubmit = () => { }
+
+    const handlechange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement >) => {
+        setInputValues({
+            ...inputValues,
+            [e.target.name]: e.target.value
+         } )
+    }
 
     return (
         <div>
             <form onSubmit={handlesubmit}>
-                <input type="text" name="nick" placeholder="nick" />
-                <input type="text" name="subMonths" placeholder="subMonths" />
-                <input type="text" name="description" placeholder="description" />
-                <input type="text" name="avatar" placeholder="avatar" />
+                <input onChange={handlechange} value={inputValues.nick} type="text" name="nick" placeholder="nick" />
+                <input onChange={handlechange} value={inputValues.subMonths} type="text" name="subMonths" placeholder="subMonths" />
+                <textarea onChange={handlechange} value={inputValues.description}  name="description" placeholder="description" />
+                <input onChange={handlechange} value={inputValues.avatar} type="text" name="avatar" placeholder="avatar" />
             </form>
 
             <button>Save a new sub</button>
